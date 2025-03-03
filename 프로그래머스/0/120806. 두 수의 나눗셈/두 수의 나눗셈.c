76cc7a962f69b78a12add5787f6614ebb3b94a52
hyerim <164465335+hyerimmmmm@users.noreply.github.com>
2025-03-04 08:36:37 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    
+    answer = (float)num1 / (float)num2 * 1000;
+    return (int)answer;
+}
