fb347cfb37f85660cf5dfede1ba10be573e2026d
hyerim <164465335+hyerimmmmm@users.noreply.github.com>
2025-03-06 16:00:35 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int messageNum = message.length();
+		
+		int width = messageNum * 2;
+        return width;
+    }
+}
