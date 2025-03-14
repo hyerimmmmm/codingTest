5697c0b757645d41a4889106abda72308f77b51b
hyerim <164465335+hyerimmmmm@users.noreply.github.com>
2025-03-14 15:27:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.95 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int n) {
+        int count = 0;
+            int count2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        
+        int[] answer = new int[count];
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer[count2] = i;
+                count2++;
+            }
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
