fb4174f83c9d78e139efb5f38527bd372df1f07a
hyerim <164465335+hyerimmmmm@users.noreply.github.com>
2025-03-18 17:28:42 +0900

    [level 0] Title: 암호 해독, Time: 21.46 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for (int i = 1; i <= cipher.length(); i++) {
+            if (i % code == 0) {
+                answer += cipher.charAt(i - 1);
+                System.out.println(i);
+            }
+        }
+        
+        return answer;
+    }
+}
