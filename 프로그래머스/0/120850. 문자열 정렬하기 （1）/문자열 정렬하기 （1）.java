fd4efab7c7dc43796c960d82e97056d95faca4af
hyerim <164465335+hyerimmmmm@users.noreply.github.com>
2025-03-18 16:48:29 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.71 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String str = my_string.replaceAll("[^0-9]","");
+        int[] answer = new int[str.length()];
+        
+        for (int i = 0; i < str.length(); i++) {
+            answer[i] = Character.getNumericValue(str.charAt(i));
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
